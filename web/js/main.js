fc6685ff490b91b71025a059e98e5ceb92749f03
felipepereira <felipepereira2697@gmail.com>
2019-01-29 18:13:25 -0200

    adding rslides plugin to pages

---
+++ web/js/main.js
@@ -0,0 +1,17 @@
+$(function() {
+    $(".rslides").responsiveSlides();
+});
+
+$(".rslides").responsiveSlides({
+    auto: true,             // Boolean: Animate automatically, true or false
+    speed: 500,            // Integer: Speed of the transition, in milliseconds
+    timeout: 3000,          // Integer: Time between slide transitions, in milliseconds
+});
+
+$(".rslidesPortfolio").responsiveSlides({
+    auto: true,             // Boolean: Animate automatically, true or false
+    speed: 500,            // Integer: Speed of the transition, in milliseconds
+    timeout: 4000,          // Integer: Time between slide transitions, in milliseconds
+    pager: true,           // Boolean: Show pager, true or false
+
+  });
